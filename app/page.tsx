9d8fe73052b2a9d41732ca374f789a6ca28b8373
Geher-Marcell <darkiex03@gmail.com>
2025-12-01 22:04:22 +0100

    TODO: - Circle's z-index - Navbar on mobile view CHANGES: - Added new fonts - Added placeholder sections - Other minor stuff

--- app/page.tsx
+++ app/page.tsx
@@ -15,34 +15,36 @@ export default function Home() {
 						<main className="relative flex flex-col p-6 sm:p-16 gap-10">
 							<div className="w-full h-full" id="welcomePage">
 								<div className="relative flex min-h-[calc(100dvh-5rem)] w-full flex-col sm:min-h-[calc(100dvh-8rem)] sm:py-8 items-center justify-center border-b border-(--foreground-color)/10">
-									<Circle
+									{/* <Circle
 										backgroundGradient="linear-gradient(45deg, #ac2b2b, #db4e2aff)"
-										top={200}
+										bottom={-120}
 										right={-80}
 										blurAmount={80}
 										size={320}
 									/>
 									<Circle
 										backgroundGradient="linear-gradient(225deg, #3636adff, #1a52a7)"
-										bottom={-80}
+										top={200}
 										left={100}
 										blurAmount={80}
 										size={220}
-									/>
+									/> */}
 									<WelcomePage />
 								</div>
 							</div>
 							<div className="w-full h-full" id="aboutPage">
 								<AboutPage />
 							</div>
-							<div className="w-full h-200" id="projectsPage">
-								<h1 className="inline-block text-4xl">My projects</h1>
+							<div className="w-full h-200 pl-10" id="projectsPage">
+								<h1 className="inline-block text-4xl font-semibold">
+									My projects
+								</h1>
 								<div className="bg-red-500 inline-block rotate-30 -translate-y-6 px-1 rounded-lg font-bold">
 									WIP
 								</div>
 							</div>
-							<div className="w-full h-200" id="contactPage">
-								<h1 className="inline-block text-4xl">
+							<div className="w-full h-200 pl-10" id="contactPage">
+								<h1 className="inline-block text-4xl font-semibold">
 									Let&apos;s get in touch
 								</h1>
 								<div className="bg-red-500 inline-block rotate-30 -translate-y-6 px-1 rounded-lg font-bold">
